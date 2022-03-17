b859bfb5f29a753d282d6288479053b947a0c1f5
Arkady Pugachev <qweeex@yandex.com>
2022-03-17 14:13:45 +0400

    add script

---
+++ js/getData.js
@@ -0,0 +1,30 @@
+Number.prototype.between = function(a, b, inclusive) {
+    var min = Math.min.apply(Math, [a, b]),
+        max = Math.max.apply(Math, [a, b]);
+    return inclusive ? this >= min && this <= max : this > min && this < max;
+}
+
+async function getAroma(gender, score){
+    let data;
+    await fetch('/data.json', {
+        method: "GET"
+    })
+        .then(res => res.json())
+        .then(res => {
+            data = res;
+        });
+    let newArr = data.filter((item) => item.gender === gender)
+    let result;
+    newArr.forEach((item) => {
+        if (score.between(item.scoreMin, item.scoreMax, true)){
+            result = item
+        }
+    })
+    return result
+}
+
+
+// test
+getAroma('IT MATTERS NOT', 19).then((res) => {
+    console.log(res)
+})
